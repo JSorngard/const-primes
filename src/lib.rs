f76ab8d7b31363c49e547c58cd75e1273b4434a3
Johanna Sörngård <jsorngard@gmail.com>
2023-09-21 16:40:00 +0200

    Simplify grammar

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! This crate defines the type [`Primes`] that is an array of primes computed at compile time.
+//! This crate defines the type [`Primes`]: an array of primes computed at compile time.
 //!
 //! The crate currently uses trial division to compute primes, and is therefore not suitable for
 //! generating large arrays.
