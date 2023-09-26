ccb8c4bc7bd68583aba45df1f15bf5da1621eee0
Johanna Sörngård <jsorngard@gmail.com>
2023-09-26 09:03:05 +0200

    Use only a single witness list

--- src/miller_rabin.rs
+++ src/miller_rabin.rs
@@ -26,33 +26,13 @@ pub const fn is_prime(n: u64) -> bool {
 
         // Since we know the maximum size of the numbers we test against we can use the fact that there are known perfect bases
         // in order to make the test both fast and deterministic.
-        // This lists of witnesses were taken from https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Testing_against_small_sets_of_bases.
-        let (witnesses, num_witnesses) = if n < 1_373_653 {
-            ([2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 2)
-        } else if n < 9_080_191 {
-            ([31, 73, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 2)
-        } else if n < 25_326_001 {
-            ([2, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0], 3)
-        } else if n < 3_215_031_751 {
-            ([2, 3, 5, 7, 0, 0, 0, 0, 0, 0, 0, 0], 4)
-        } else if n < 4_759_123_141 {
-            ([2, 7, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0], 3)
-        } else if n < 1_122_004_669_633 {
-            ([2, 13, 23, 1662803, 0, 0, 0, 0, 0, 0, 0, 0], 4)
-        } else if n < 2_152_302_898_747 {
-            ([2, 3, 5, 7, 11, 0, 0, 0, 0, 0, 0, 0], 5)
-        } else if n < 3_474_749_660_383 {
-            ([2, 3, 5, 7, 11, 13, 0, 0, 0, 0, 0, 0], 6)
-        } else if n < 341_550_071_728_321 {
-            ([2, 3, 5, 7, 11, 13, 17, 0, 0, 0, 0, 0], 7)
-        } else if n < 3_825_123_056_546_413_051 {
-            ([2, 3, 5, 7, 11, 13, 17, 19, 23, 0, 0, 0], 9)
-        } else {
-            ([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37], 12)
-        };
+        // This list of witnesses was taken from https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Testing_against_small_sets_of_bases
+        // and is sufficient for all numbers smaller than 2^64.
+        const NUM_WITNESSES: usize = 12;
+        let witnesses: [u64; NUM_WITNESSES] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
 
         let mut i = 0;
-        while i < num_witnesses {
+        while i < NUM_WITNESSES && witnesses[i] < n {
             if !miller_test(d, n, witnesses[i]) {
                 return false;
             }
@@ -109,12 +89,17 @@ const fn mod_mul(a: u64, b: u64, modulo: u64) -> u64 {
 #[cfg(test)]
 mod test {
     use super::is_prime;
-    
+
     #[test]
     fn check_is_prime() {
         #[rustfmt::skip]
         const TEST_CASES: [bool; 100] = [false, false, true, true, false, true, false, true, false, false, false, true, false, true, false, false, false, true, false, true, false, false, false, true, false, false, false, false, false, true, false, true, false, false, false, false, false, true, false, false, false, true, false, true, false, false, false, true, false, false, false, false, false, true, false, false, false, false, false, true, false, true, false, false, false, false, false, true, false, false, false, true, false, true, false, false, false, false, false, true, false, false, false, true, false, false, false, false, false, true, false, false, false, false, false, false, false, true, false, false];
         for (x, ans) in TEST_CASES.into_iter().enumerate() {
+            print!("{x} should");
+            if !ans {
+                print!(" not");
+            }
+            println!(" be prime");
             assert_eq!(is_prime(x as u64), ans);
         }
         assert_eq!(is_prime(18446744073709551557), true);
