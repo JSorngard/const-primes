51bd863361db2fba707ff29c3a7941e6287a87e1
Johanna Sörngård <jsorngard@gmail.com>
2023-09-26 17:14:22 +0200

    bench is_prime on random numbers

--- benches/prime_benches.rs
+++ benches/prime_benches.rs
@@ -1,13 +1,23 @@
 use const_primes::{are_prime, is_prime, primes};
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use rand::prelude::*;
 use std::hint::black_box;
 
 fn benchmarks(c: &mut Criterion) {
     c.bench_function("generate 10000 primes", |b| {
         b.iter(|| black_box(primes::<10_000>()))
     });
-    c.bench_function("is_prime(1000000)", |b| {
-        b.iter(|| black_box(is_prime(1_000_000)))
+
+    c.bench_function("is_prime on random numbers", |b| {
+        b.iter_batched(
+            || (0..10_000).map(|_| random()).collect::<Vec<u64>>(),
+            |data| {
+                for number in data.iter() {
+                    black_box(is_prime(*number));
+                }
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("sieve 10000 integers", |b| {
         b.iter(|| black_box(are_prime::<10_000>()))
