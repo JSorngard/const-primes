456d59209facb187e4552e94f9ea2f0ea894471b
Johanna Sörngård <jsorngard@gmail.com>
2023-09-26 08:49:00 +0200

    Only miller rabin

--- src/lib.rs
+++ src/lib.rs
@@ -74,6 +74,8 @@
 type Underlying = u32;
 
 mod wrapper;
+mod miller_rabin;
+pub use miller_rabin::is_prime;
 pub use wrapper::Primes;
 
 /// Returns the largest integer smaller than or equal to √n.
@@ -240,7 +242,7 @@ pub const fn are_prime<const N: usize>() -> [bool; N] {
 }
 
 /// Returns the largest prime smaller than or equal to `n` if there is one.
-/// 
+///
 /// # Examples
 /// ```
 /// # use const_primes::largest_prime_leq;
@@ -266,14 +268,14 @@ pub const fn largest_prime_leq(mut n: u64) -> Option<u64> {
         while !is_prime(n) {
             n -= 2;
         }
-        
+
         Some(n)
     }
 }
 
 /// Returns the smallest prime larger than or equal to `n` if there is one that
 /// can be represented by a `u64`.
-/// 
+///
 /// # Example
 /// ```
 /// # use const_primes::smallest_prime_geq;
@@ -329,123 +331,6 @@ pub const fn prime_counts<const N: usize>() -> [usize; N] {
         i += 1;
     }
     counts
-}
-
-/// Returns whether `n` is prime.
-///
-/// Uses trial division with a small wheel for small numbers and the
-/// Miller-Rabin primality test with known perfect witness bases for large numbers.
-///
-/// # Example
-/// Basic usage
-/// ```
-/// # use const_primes::is_prime;
-/// const CHECK: bool = is_prime(18_446_744_073_709_551_557);
-/// assert!(CHECK);
-/// ```
-#[must_use]
-pub const fn is_prime(n: u64) -> bool {
-    if n == 2 || n == 3 {
-        true
-    } else if n <= 1 || n % 2 == 0 || n % 3 == 0 {
-        false
-    } else if n <= u16::MAX as u64 {
-        let mut candidate_factor = 5;
-        let bound = isqrt(n) + 1;
-        while candidate_factor < bound {
-            if n % candidate_factor == 0 || n % (candidate_factor + 2) == 0 {
-                return false;
-            }
-            candidate_factor += 6;
-        }
-        true
-    } else {
-        // Find r such that n = 2^d * r + 1 for some r >= 1
-        let mut d = n - 1;
-        while d % 2 == 0 {
-            d /= 2;
-        }
-
-        // Since we know the maximum size of the numbers we test against we can use the fact that there are known perfect bases
-        // in order to make the test both fast and deterministic.
-        // These lists of witnesses were taken from https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Testing_against_small_sets_of_bases.
-        let (witnesses, num_witnesses) = if n < 1_373_653 {
-            ([2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 2)
-        } else if n < 9_080_191 {
-            ([31, 73, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 2)
-        } else if n < 25_326_001 {
-            ([2, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0], 3)
-        } else if n < 3_215_031_751 {
-            ([2, 3, 5, 7, 0, 0, 0, 0, 0, 0, 0, 0], 4)
-        } else if n < 4_759_123_141 {
-            ([2, 7, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0], 3)
-        } else if n < 1_122_004_669_633 {
-            ([2, 13, 23, 1662803, 0, 0, 0, 0, 0, 0, 0, 0], 4)
-        } else if n < 2_152_302_898_747 {
-            ([2, 3, 5, 7, 11, 0, 0, 0, 0, 0, 0, 0], 5)
-        } else if n < 3_474_749_660_383 {
-            ([2, 3, 5, 7, 11, 13, 0, 0, 0, 0, 0, 0], 6)
-        } else if n < 341_550_071_728_321 {
-            ([2, 3, 5, 7, 11, 13, 17, 0, 0, 0, 0, 0], 7)
-        } else if n < 3_825_123_056_546_413_051 {
-            ([2, 3, 5, 7, 11, 13, 17, 19, 23, 0, 0, 0], 9)
-        } else {
-            ([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37], 12)
-        };
-
-        let mut i = 0;
-        while i < num_witnesses {
-            if !miller_test(d, n, witnesses[i]) {
-                return false;
-            }
-            i += 1;
-        }
-
-        true
-    }
-}
-
-/// Performs a Miller-Rabin test with the witness k.
-const fn miller_test(mut d: u64, n: u64, k: u64) -> bool {
-    let mut x = mod_pow(k, d, n);
-    if x == 1 || x == n - 1 {
-        return true;
-    }
-
-    while d != n - 1 {
-        x = mod_mul(x, x, n);
-        d *= 2;
-
-        if x == 1 {
-            return false;
-        } else if x == n - 1 {
-            return true;
-        }
-    }
-
-    false
-}
-
-/// Returns (a ^ b) % m.
-const fn mod_pow(mut base: u64, mut exp: u64, modulo: u64) -> u64 {
-    let mut res = 1;
-
-    base %= modulo;
-
-    while exp > 0 {
-        if exp % 2 == 1 {
-            res = mod_mul(res, base, modulo);
-        }
-        base = mod_mul(base, base, modulo);
-        exp >>= 1;
-    }
-
-    res
-}
-
-/// Calculates (a * b) % m without overflow.
-const fn mod_mul(a: u64, b: u64, modulo: u64) -> u64 {
-    ((a as u128 * b as u128) % modulo as u128) as u64
 }
 
 #[cfg(test)]
---
+++ src/miller_rabin.rs
@@ -0,0 +1,88 @@
+//! This module contains an implementation of a deterministic Miller-Rabin primality test
+
+/// Returns whether `n` is prime.
+///
+/// Uses a Miller-Rabin primality test with a known perfect witness base numbers.
+///
+/// # Example
+/// Basic usage
+/// ```
+/// # use const_primes::is_prime;
+/// const CHECK: bool = is_prime(18_446_744_073_709_551_557);
+/// assert!(CHECK);
+/// ```
+#[must_use]
+pub const fn is_prime(n: u64) -> bool {
+    if n == 2 || n == 3 {
+        true
+    } else if n <= 1 || n % 2 == 0 || n % 3 == 0 {
+        false
+    } else {
+        // Find r such that n = 2^d * r + 1 for some r >= 1
+        let mut d = n - 1;
+        while d % 2 == 0 {
+            d /= 2;
+        }
+
+        // Since we know the maximum size of the numbers we test against we can use the fact that there are known perfect bases
+        // in order to make the test both fast and deterministic.
+        // This lists of witnesses was taken from https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Testing_against_small_sets_of_bases.
+        const NUM_WITNESSES: usize = 12;
+        let witnesses: [u64; NUM_WITNESSES] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
+
+        let mut i = 0;
+        while i < NUM_WITNESSES {
+            if !miller_test(d, n, witnesses[i]) {
+                return false;
+            }
+            i += 1;
+        }
+
+        true
+    }
+}
+
+/// Performs a Miller-Rabin test with the witness k.
+const fn miller_test(mut d: u64, n: u64, k: u64) -> bool {
+    let mut x = mod_pow(k, d, n);
+    if x == 1 || x == n - 1 {
+        return true;
+    }
+
+    while d != n - 1 {
+        x = mod_mul(x, x, n);
+        d *= 2;
+
+        if x == 1 {
+            return false;
+        } else if x == n - 1 {
+            return true;
+        }
+    }
+
+    false
+}
+
+/// Returns (a ^ b) % m.
+const fn mod_pow(mut base: u64, mut exp: u64, modulo: u64) -> u64 {
+    let mut res = 1;
+
+    base %= modulo;
+
+    while exp > 0 {
+        if exp % 2 == 1 {
+            res = mod_mul(res, base, modulo);
+        }
+        base = mod_mul(base, base, modulo);
+        exp >>= 1;
+    }
+
+    res
+}
+
+/// Calculates (a * b) % m without overflow.
+const fn mod_mul(a: u64, b: u64, modulo: u64) -> u64 {
+    // Since a and b are u64s the product is guaranteed to fit in a u128,
+    // and since modulo is a u64 the downcast at the end is guaranteed to be safe
+    ((a as u128 * b as u128) % modulo as u128) as u64
+}
