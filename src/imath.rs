b9ea6f20b0629c14a28256ccab07cb3526c985c4
Johanna Sörngård <jsorngard@gmail.com>
2023-09-27 09:42:38 +0200

    Add must_use to modulo functins

--- src/imath.rs
+++ src/imath.rs
@@ -25,6 +25,7 @@ pub const fn isqrt(n: u64) -> u64 {
 }
 
 /// Calculates (a ^ b) % m without overflow.
+#[must_use]
 pub const fn mod_pow(mut base: u64, mut exp: u64, modulo: u64) -> u64 {
     let mut res = 1;
 
@@ -42,6 +43,7 @@ pub const fn mod_pow(mut base: u64, mut exp: u64, modulo: u64) -> u64 {
 }
 
 /// Calculates (a * b) % m without overflow.
+#[must_use]
 pub const fn mod_mul(a: u64, b: u64, modulo: u64) -> u64 {
     ((a as u128 * b as u128) % modulo as u128) as u64
 }
