011aa7b8ef62fbee916d1229f77758a08aeaba1c
Johanna Sörngård <jsorngard@gmail.com>
2023-10-27 11:01:42 +0200

    Name benchmark groups programmatically

--- benches/prime_benches.rs
+++ benches/prime_benches.rs
@@ -1,44 +1,54 @@
-use const_primes::{is_prime, primes, primes_greater_than_or_equal_to, primes_less_than, sieve, sieve_less_than, sieve_greater_than_or_equal_to};
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use const_primes::{
+    is_prime, primes, primes_greater_than_or_equal_to, primes_less_than, sieve,
+    sieve_greater_than_or_equal_to, sieve_less_than,
+};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion, Throughput};
 use rand::prelude::*;
 use std::hint::black_box;
 
 fn benchmarks(c: &mut Criterion) {
     {
+        const N: usize = 10_000;
         let mut prime_generation = c.benchmark_group("prime generation");
-        prime_generation.bench_function("first 10000 primes", |b| {
-            b.iter(|| black_box(primes::<10_000>()))
+        prime_generation.bench_function(format!("first {N} primes"), |b| {
+            b.iter(|| black_box(primes::<N>()))
         });
-        prime_generation.bench_function("10000 primes < 100000000", |b| {
-            b.iter(|| black_box(primes_less_than::<10000>(100000000)))
+        prime_generation.bench_function(format!("{N} primes < 100000000"), |b| {
+            b.iter(|| black_box(primes_less_than::<N>(100000000)))
         });
-        prime_generation.bench_function("10000 primes >= 99990000", |b| {
-            b.iter(|| black_box(primes_greater_than_or_equal_to::<10000>(99990000)))
+        prime_generation.bench_function(format!("{N} primes >= 99990000"), |b| {
+            b.iter(|| black_box(primes_greater_than_or_equal_to::<N>(99990000)))
         });
     }
 
-    c.bench_function("is_prime on 10000 random numbers", |b| {
-        b.iter_batched(
-            || (0..10_000).map(|_| random()).collect::<Vec<u64>>(),
-            |data| {
-                for number in data.iter() {
-                    black_box(is_prime(*number));
-                }
-            },
-            BatchSize::SmallInput,
-        )
-    });
+    {
+        const N: u64 = 10_000;
+        let mut primality_testing = c.benchmark_group("primality testing");
+        primality_testing.throughput(Throughput::Elements(N));
+        primality_testing.bench_function(format!("is_prime on {N} random numbers"), |b| {
+            b.iter_batched(
+                || (0..N).map(|_| random()).collect::<Vec<u64>>(),
+                |data| {
+                    for number in data.iter() {
+                        black_box(is_prime(*number));
+                    }
+                },
+                BatchSize::SmallInput,
+            )
+        });
+    }
 
     {
+        const N: usize = 10_000;
         let mut sieving = c.benchmark_group("prime sieving");
-        sieving.bench_function("first 10000 integers", |b| {
-            b.iter(|| black_box(sieve::<10_000>()))
+        sieving.bench_function(format!("first {N} integers"), |b| {
+            b.iter(|| black_box(sieve::<N>()))
         });
-        sieving.bench_function("10000 integers < 100000000", |b| {
-            b.iter(|| black_box(sieve_less_than::<10_000>(100000000)))
+        sieving.bench_function(format!("{N} integers < 100000000"), |b| {
+            b.iter(|| black_box(sieve_less_than::<N>(100000000)))
         });
-        sieving.bench_function("10000 integers >= 99990000", |b| {
-            b.iter(|| black_box(sieve_greater_than_or_equal_to::<10_000>(99990000)))
+        sieving.bench_function(format!("{N} integers >= 99990000"), |b| {
+            b.iter(|| black_box(sieve_greater_than_or_equal_to::<N>(99990000)))
         });
     }
 }
