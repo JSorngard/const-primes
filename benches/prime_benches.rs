ae2c7bff5f655b7542b3ff63e5c10c90294c92fc
Johanna Sörngård <jsorngard@gmail.com>
2023-10-27 10:52:05 +0200

    Add benchmark to all sieve functions

--- benches/prime_benches.rs
+++ benches/prime_benches.rs
@@ -1,4 +1,4 @@
-use const_primes::{is_prime, primes, primes_greater_than_or_equal_to, primes_less_than, sieve};
+use const_primes::{is_prime, primes, primes_greater_than_or_equal_to, primes_less_than, sieve, sieve_less_than, sieve_greater_than_or_equal_to};
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use rand::prelude::*;
 use std::hint::black_box;
@@ -17,7 +17,7 @@ fn benchmarks(c: &mut Criterion) {
         });
     }
 
-    c.bench_function("is_prime on random numbers", |b| {
+    c.bench_function("is_prime on 10000 random numbers", |b| {
         b.iter_batched(
             || (0..10_000).map(|_| random()).collect::<Vec<u64>>(),
             |data| {
@@ -29,9 +29,18 @@ fn benchmarks(c: &mut Criterion) {
         )
     });
 
-    c.bench_function("sieve 10000 integers", |b| {
-        b.iter(|| black_box(sieve::<10_000>()))
-    });
+    {
+        let mut sieving = c.benchmark_group("prime sieving");
+        sieving.bench_function("first 10000 integers", |b| {
+            b.iter(|| black_box(sieve::<10_000>()))
+        });
+        sieving.bench_function("10000 integers < 100000000", |b| {
+            b.iter(|| black_box(sieve_less_than::<10_000>(100000000)))
+        });
+        sieving.bench_function("10000 integers >= 99990000", |b| {
+            b.iter(|| black_box(sieve_greater_than_or_equal_to::<10_000>(99990000)))
+        });
+    }
 }
 
 criterion_group!(benches, benchmarks);
