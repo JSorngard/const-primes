fd8567e5524ee33e240e47f675a4f9670782d9c8
Johanna Sörngård <jsorngard@gmail.com>
2023-10-27 10:48:14 +0200

    Add benchmark to all prime generation functions

--- benches/prime_benches.rs
+++ benches/prime_benches.rs
@@ -1,12 +1,21 @@
-use const_primes::{is_prime, primes, sieve};
+use const_primes::{is_prime, primes, primes_greater_than_or_equal_to, primes_less_than, sieve};
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use rand::prelude::*;
 use std::hint::black_box;
 
 fn benchmarks(c: &mut Criterion) {
-    c.bench_function("generate 10000 primes", |b| {
-        b.iter(|| black_box(primes::<10_000>()))
-    });
+    {
+        let mut prime_generation = c.benchmark_group("prime generation");
+        prime_generation.bench_function("first 10000 primes", |b| {
+            b.iter(|| black_box(primes::<10_000>()))
+        });
+        prime_generation.bench_function("10000 primes < 100000000", |b| {
+            b.iter(|| black_box(primes_less_than::<10000>(100000000)))
+        });
+        prime_generation.bench_function("10000 primes >= 99990000", |b| {
+            b.iter(|| black_box(primes_greater_than_or_equal_to::<10000>(99990000)))
+        });
+    }
 
     c.bench_function("is_prime on random numbers", |b| {
         b.iter_batched(
