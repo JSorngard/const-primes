345f637e167b29a2243c5efb580e15daee82d321
Johanna Sörngård <jsorngard@gmail.com>
2024-05-05 08:47:04 +0200

    Add docstring to array_section

--- src/array_section.rs
+++ src/array_section.rs
@@ -178,6 +178,7 @@ impl<const N: usize, T> ArraySection<T, N> {
 pub struct TryFromArraySectionError<T, const N: usize>(ArraySection<T, N>);
 
 impl<T, const N: usize> TryFromArraySectionError<T, N> {
+    /// Returns the original [`ArraySection`].
     pub fn array_section(self) -> ArraySection<T, N> {
         self.0
     }
