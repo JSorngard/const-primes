44da96f2c43f53e27e2535cbf498749bafce5884
Johanna Sörngård <jsorngard@gmail.com>
2024-05-30 16:11:07 +0200

    whitespace in crate docstring

--- src/lib.rs
+++ src/lib.rs
@@ -49,6 +49,7 @@
 //! assert!(CACHE.is_prime(1000).is_none());
 //! assert!(CACHE.count_primes_leq(1000).is_none());
 //! ```
+//!
 //! # Example: primality checking
 //!
 //! Use [`is_prime`] to test whether a given number is prime:
@@ -74,7 +75,8 @@
 //!
 //! # Example: generate the three primes after 5000000031
 //!
-//! The crate also provides prime generation and sieving functions that can be used to work with ranges that don't start at zero, e.g. [`primes_geq`] and [`sieve_lt`]. These functions can use large sieves to compute large primes, but don't need to return the entire sieve, just the requested numbers.
+//! The crate also provides prime generation and sieving functions that can be used to work with ranges that don't start at zero, e.g. [`primes_geq`] and [`sieve_lt`].
+//! These functions can use large sieves to compute large primes, but don't need to return the entire sieve, just the requested numbers.
 //! They are most conveniently used through the macros [`primes_segment!`] and [`sieve_segment!`] that automatically compute the size of the sieve that's needed for a certain computation.
 //!
 //! Compute 3 primes greater than or equal to 5000000031:
