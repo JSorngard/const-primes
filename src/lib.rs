57ed8bc348944cc3747ae3f1c5c5aa73e83fd282
Johanna Sörngård <jsorngard@gmail.com>
2024-06-17 10:48:37 +0200

    activating --> enabling

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! This crate lets you for example pre-compute prime numbers at compile time, store them in the binary, and use them later for related computations,
 //! or check whether a number is prime in a const function.
 //!
-//! `#![no_std]` compatible, and currently supports Rust versions 1.67.1 or newer, though activating feature flags may increase this.
+//! `#![no_std]` compatible, and currently supports Rust versions 1.67.1 or newer, though enabling feature flags may increase this.
 //!
 //! # Example: generate primes at compile time
 //!
