de517b1830d46399595123f6a95e66eac30b21b3
Johanna Sörngård <jsorngard@gmail.com>
2024-06-17 10:45:50 +0200

    better test name

--- src/cache/mod.rs
+++ src/cache/mod.rs
@@ -953,7 +953,7 @@ mod test {
     #[cfg(not(feature = "const_assert"))]
     #[test]
     #[should_panic]
-    fn check_panic() {
+    fn check_panic_on_empty_new() {
         let _primes = Primes::<0>::new();
     }
 }
