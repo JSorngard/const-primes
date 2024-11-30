05d04a2ffee37ac2c3b781f443d5b8e915a89914
Johanna Sörngård <jsorngard@gmail.com>
2024-11-30 18:20:25 +0100

    Split benchmark into slow and fast
    The idea is now to do a binary search over values of `N` until we find the value for which mapping `is_prime` becomes faster than sieving.

--- benches/find_crossover.rs
+++ benches/find_crossover.rs
@@ -4,32 +4,55 @@ use const_primes::{is_prime, sieve_geq};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn find_crossover(c: &mut Criterion) {
-    // An N larger than this results in a stack overflow in sieve_geq
-    const N: usize = 253099;
+    const N_FAST: usize = 10;
 
-    let mut group = c.benchmark_group("crossover");
+    // A MEM larger than this risks a stack overflow in sieve_geq
+    // sieve_geq always takes the same ammount of time for a given MEM.
+    const MEM: usize = 250000;
 
-    for lower_limit in [1000000, 100000000, (N * N - N) as u64] {
-        // sieve_geq always takes the same ammount of time for a given MEM.
-        group.bench_function(format!("sieve_geq({lower_limit})"), |b| {
-            b.iter(|| {
-                black_box(sieve_geq::<N, N>(lower_limit)).unwrap();
-            })
+    // As large as possible for the given MEM.
+    const N_SLOW: usize = MEM;
+
+    // Start as high up as possible
+    let lower_limit = (MEM * MEM - MEM) as u64;
+
+    let mut fast_group = c.benchmark_group("fast");
+
+    fast_group.bench_function(format!("sieve_geq({lower_limit})"), |b| {
+        b.iter(|| {
+            black_box(sieve_geq::<N_FAST, MEM>(lower_limit)).unwrap();
+        })
+    });
+
+    fast_group.bench_function(format!("map_is_prime({lower_limit})"), |b| {
+        b.iter(|| {
+            black_box(
+                (lower_limit..lower_limit + N_FAST as u64)
+                    .map(is_prime)
+                    .collect::<Vec<_>>(),
+            )
         });
-        
-        group.bench_function(
-            format!("map_is_prime({lower_limit})"),
-            |b| {
-                b.iter(|| {
-                    black_box(
-                        (lower_limit..lower_limit + N as u64)
-                            .map(is_prime)
-                            .collect::<Vec<_>>(),
-                    )
-                });
-            },
-        );
-    }
+    });
+
+    drop(fast_group);
+
+    let mut slow_group = c.benchmark_group("slow");
+
+    slow_group.bench_function(format!("sieve_geq({lower_limit})"), |b| {
+        b.iter(|| {
+            black_box(sieve_geq::<N_SLOW, MEM>(lower_limit)).unwrap();
+        });
+    });
+
+    slow_group.bench_function(format!("map_is_prime({lower_limit})"), |b| {
+        b.iter(|| {
+            black_box(
+                (lower_limit..lower_limit + N_SLOW as u64)
+                    .map(is_prime)
+                    .collect::<Vec<_>>(),
+            )
+        });
+    });
 }
 
 criterion_group!(crossover, find_crossover);
