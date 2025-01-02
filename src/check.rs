384264c608c8c087bffec79e2cb987ef3b78375e
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2025-01-02 23:25:15 +0100

    Re-add the `fast_test` feature (#101)
    * Add back the `fast_test` feature
    
    * Switch to small feature from tiny
    
    * update machine-prime
    
    * Document feature
    
    * Update machine prime and add more documentation
    
    * Bump version number

--- src/check.rs
+++ src/check.rs
@@ -1,4 +1,6 @@
 //! This module contains an implementation of a deterministic Miller-Rabin primality test
+
+#[cfg(not(feature = "fast_test"))]
 use crate::integer_math::{mod_mul, mod_pow};
 
 /// Returns whether `n` is prime.
@@ -17,66 +19,75 @@ use crate::integer_math::{mod_mul, mod_pow};
 /// ```
 #[must_use]
 pub const fn is_prime(n: u64) -> bool {
-    // Since we know the maximum size of the numbers we test against
-    // we can use the fact that there are known perfect bases
-    // in order to make the test both fast and deterministic.
-    // This list of witnesses was taken from
-    // <https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Testing_against_small_sets_of_bases>.
-    const NUM_BASES: usize = 11;
-    const WITNESSES: [(u64, &[u64]); NUM_BASES] = [
-        (2_046, &[2]),
-        (1_373_652, &[2, 3]),
-        (9_080_190, &[31, 73]),
-        (25_326_000, &[2, 3, 5]),
-        (4_759_123_140, &[2, 7, 61]),
-        (1_112_004_669_632, &[2, 13, 23, 1_662_803]),
-        (2_152_302_898_746, &[2, 3, 5, 7, 11]),
-        (3_474_749_660_382, &[2, 3, 5, 7, 11, 13]),
-        (341_550_071_728_320, &[2, 3, 5, 7, 11, 13, 17]),
-        (3_825_123_056_546_413_050, &[2, 3, 5, 7, 11, 13, 17, 19, 23]),
-        (u64::MAX, &[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]),
-    ];
-
-    if n == 2 || n == 3 {
-        return true;
-    } else if n <= 1 || n % 2 == 0 || n % 3 == 0 {
-        return false;
+    #[cfg(feature = "fast_test")]
+    {
+        machine_prime::is_prime(n)
     }
 
-    // Use a small wheel to check up to log2(n).
-    // This keeps the complexity at O(log(n)).
-    let mut candidate_factor = 5;
-    let trial_limit = n.ilog2() as u64;
-    while candidate_factor <= trial_limit {
-        if n % candidate_factor == 0 || n % (candidate_factor + 2) == 0 {
+    #[cfg(not(feature = "fast_test"))]
+    {
+        // Since we know the maximum size of the numbers we test against
+        // we can use the fact that there are known perfect bases
+        // in order to make the test both fast and deterministic.
+        // This list of witnesses was taken from
+        // <https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Testing_against_small_sets_of_bases>.
+        const NUM_BASES: usize = 11;
+        const WITNESSES: [(u64, &[u64]); NUM_BASES] = [
+            (2_046, &[2]),
+            (1_373_652, &[2, 3]),
+            (9_080_190, &[31, 73]),
+            (25_326_000, &[2, 3, 5]),
+            (4_759_123_140, &[2, 7, 61]),
+            (1_112_004_669_632, &[2, 13, 23, 1_662_803]),
+            (2_152_302_898_746, &[2, 3, 5, 7, 11]),
+            (3_474_749_660_382, &[2, 3, 5, 7, 11, 13]),
+            (341_550_071_728_320, &[2, 3, 5, 7, 11, 13, 17]),
+            (3_825_123_056_546_413_050, &[2, 3, 5, 7, 11, 13, 17, 19, 23]),
+            (u64::MAX, &[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]),
+        ];
+
+        if n == 2 || n == 3 {
+            return true;
+        } else if n <= 1 || n % 2 == 0 || n % 3 == 0 {
             return false;
         }
-        candidate_factor += 6;
-    }
 
-    // Find r such that n = 2^d * r + 1 for some r >= 1
-    let mut d = n - 1;
-    while d % 2 == 0 {
-        d >>= 1;
-    }
-
-    let mut i = 0;
-    while i < NUM_BASES && WITNESSES[i].0 < n {
-        i += 1;
-    }
-    let witnesses = WITNESSES[i].1;
-
-    let mut i = 0;
-    while i < witnesses.len() && witnesses[i] < n {
-        if !miller_test(d, n, witnesses[i]) {
-            return false;
+        // Use a small wheel to check up to log2(n).
+        // This keeps the complexity at O(log(n)).
+        let mut candidate_factor = 5;
+        let trial_limit = n.ilog2() as u64;
+        while candidate_factor <= trial_limit {
+            if n % candidate_factor == 0 || n % (candidate_factor + 2) == 0 {
+                return false;
+            }
+            candidate_factor += 6;
         }
-        i += 1;
-    }
 
-    true
+        // Find r such that n = 2^d * r + 1 for some r >= 1
+        let mut d = n - 1;
+        while d % 2 == 0 {
+            d >>= 1;
+        }
+
+        let mut i = 0;
+        while i < NUM_BASES && WITNESSES[i].0 < n {
+            i += 1;
+        }
+        let witnesses = WITNESSES[i].1;
+
+        let mut i = 0;
+        while i < witnesses.len() && witnesses[i] < n {
+            if !miller_test(d, n, witnesses[i]) {
+                return false;
+            }
+            i += 1;
+        }
+
+        true
+    }
 }
 
+#[cfg(not(feature = "fast_test"))]
 /// Performs a Miller-Rabin test with the witness k.
 const fn miller_test(mut d: u64, n: u64, k: u64) -> bool {
     let mut x = mod_pow(k, d, n);
